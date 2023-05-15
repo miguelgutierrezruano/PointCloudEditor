e77353c0e3251fc9ed1ba7eee55587e4f1330f7f
miguelguti <miguelgutierrezruano@gmail.com>
2023-05-15 18:58:45 +0200

    linked dll!

--- PointCloudEditor/code/headers/PointCloudEditor.h
+++ PointCloudEditor/code/headers/PointCloudEditor.h
@@ -3,9 +3,18 @@
 #include <QtWidgets/QMainWindow>
 #include "ui_PointCloudEditor.h"
 
+#include "CameraController.h"
+
+using namespace mpc;
+
 class PointCloudEditor : public QMainWindow
 {
     Q_OBJECT
+
+private:
+
+    Camera cam;
+    CameraController camController;
 
 public:
     PointCloudEditor(QWidget *parent = nullptr);
--- PointCloudEditor/code/sources/PointCloudEditor.cpp
+++ PointCloudEditor/code/sources/PointCloudEditor.cpp
@@ -1,9 +1,29 @@
+#include <glm/glm.hpp>
+
 #include "PointCloudEditor.h"
 
 PointCloudEditor::PointCloudEditor(QWidget *parent)
-    : QMainWindow(parent)
+    : QMainWindow(parent),
+    camController(&cam)
 {
     ui.setupUi(this);
+
+    cam.say_hello();
+
+	camController.print_camera_position();
+	camController.set_camera_position(0, 0, 0);
+	camController.print_camera_position();
+
+	float target_fps = 1 / 60.f;
+
+	for (int i = 0; i < 100; i++)
+	{
+		camController.moveCamera(target_fps);
+	}
+
+	float advanced = cam.get_transform().get_position().z;
+	std::string text = "I advanced";
+	ui.label->setText(text.c_str());
 }
 
 PointCloudEditor::~PointCloudEditor()
