71dd7f2456ba2131c281fe23528aca3372dd6635
miguelguti <miguelgutierrezruano@gmail.com>
2023-05-15 21:37:39 +0200

    done some ui

--- PointCloudEditor/code/sources/PointCloudEditor.cpp
+++ PointCloudEditor/code/sources/PointCloudEditor.cpp
@@ -7,23 +7,6 @@ PointCloudEditor::PointCloudEditor(QWidget *parent)
     camController(&cam)
 {
     ui.setupUi(this);
-
-    cam.say_hello();
-
-	camController.print_camera_position();
-	camController.set_camera_position(0, 0, 0);
-	camController.print_camera_position();
-
-	float target_fps = 1 / 60.f;
-
-	for (int i = 0; i < 100; i++)
-	{
-		camController.moveCamera(target_fps);
-	}
-
-	float advanced = cam.get_transform().get_position().z;
-	std::string text = "I advanced";
-	ui.label->setText(text.c_str());
 }
 
 PointCloudEditor::~PointCloudEditor()
